89595b20c65d6e646bbdfe7c07712f2122fe8928
AmaanAhm <ahm.amaan@gmail.com>
2023-03-08 16:13:10 +0530

    validation

--- src/controller/register.js
+++ src/controller/register.js
@@ -1,8 +1,18 @@
-const bcrypt = require('bcrypt');
 const User = require('../models/UserModel');
+const bcrypt = require('bcrypt');
+const registerValidations = require('../validation/registerValidation');
 
 const register = async (request, response) => {
+    console.log(request.body);
     try {
+        const errors = registerValidations(request);
+        if (!errors.notEmpty()) {
+            return response.status(400).json({
+                errors: errors.array(),
+            });
+        }
+
+        // Request body is valid, continue with registration process
         const { first_name, last_name, email, phone_number, password } = request.body;
         const existingUser = await User.findOne({ email });
 
--- src/validation/registerValidation.js
+++ src/validation/registerValidation.js
@@ -1,13 +1,15 @@
-const { body, validationResult } = require('express-validator');
-// const bcrypt = require('bcrypt');
-const User = require('../models/user');
+const { body } = require('express-validator');
 
-const registerValidations = [
+const registerValidations = () => {
+
+  return [
+    
     body('first_name').notEmpty().withMessage('First name is required'),
     body('last_name').notEmpty().withMessage('Last name is required'),
     body('email').isEmail().withMessage('Invalid email address'),
     body('phone_number').isMobilePhone().withMessage('Invalid phone number'),
     body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
   ];
+};
 
 module.exports = registerValidations;

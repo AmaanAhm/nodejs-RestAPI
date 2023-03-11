dd182eb1e1ca5981c170fbdc3f20957a960a2e1f
suhail ahmad <suhail.a@icubeswire.com>
2023-03-11 13:59:41 +0530

    fixes

--- src/schema/userSchema.js
+++ src/schema/userSchema.js
@@ -1,28 +1,34 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    first_name: {
-        type: String,
-        required: true,
-        trim: true,
+const userSchema = new mongoose.Schema(
+    {
+        first_name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        last_name: {
+            type: String,
+            required: true,
+            lowercase: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        phone_number: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
     },
-    last_name: {
-        type: String,
-        required: true,
-        lowercase: true,
+    {
+        timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+        versionKey: false,
     },
-    email: {
-        type: String,
-        required: true,
-    },
-    phone_number: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-}, {timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }});
+);
 
 module.exports = userSchema;
--- src/seeder/userSeeder.js
+++ src/seeder/userSeeder.js
@@ -2,25 +2,26 @@ const mongoose = require('mongoose');
 const { faker } = require('@faker-js/faker');
 const connectDb = require('../core/db_conn');
 const userSchema = require('../schema/userSchema');
+const bcrypt = require('bcrypt');
 
 connectDb();
 
 const User = mongoose.model('User', userSchema);
 async function seed() {
-  await User.deleteMany({});
-  for (let i = 0; i < 10; i++) {
-    const user = new User({
-      first_name: faker.name.firstName(),
-      last_name: faker.name.lastName(),
-      email: faker.internet.email(),
-      phone_number: faker.phone.number('##########'),
-      password: 'Qwerty@123',
-    });
-    await user.save();
-  }
+    await User.deleteMany({});
+    for (let i = 0; i < 10; i++) {
+        const user = new User({
+            first_name: faker.name.firstName(),
+            last_name: faker.name.lastName(),
+            email: faker.internet.email(),
+            phone_number: faker.phone.number('##########'),
+            password: await bcrypt.hash('Qwerty@123', 10),
+        });
+        await user.save();
+    }
 
-  console.log('Users seeded successfully');
-  return true;
+    console.log('Users seeded successfully');
+    return true;
 }
 
 module.exports = { seed };

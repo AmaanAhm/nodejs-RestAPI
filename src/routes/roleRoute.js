ed3269f94dca95b5820e7754d80a2af789245b69
suhail ahmad <suhail.a@icubeswire.com>
2023-03-15 22:42:18 +0530

    auth

--- src/routes/roleRoute.js
+++ src/routes/roleRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const auth = require("../middleware/auth");
 
 const roleController = require('../controller/role');
 
-router.get('/role', roleController.getRoles);
+router.get('/role', auth, roleController.getRoles);
 router.post('/role', roleController.createRole);
 router.put('/role/:id', roleController.update);
 router.delete('/role/:id', roleController.deleteRole);

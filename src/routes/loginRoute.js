ef4129ecc0ca0cad9cd41f76f933a2ba86ae36d2
suhail ahmad <suhail.a@icubeswire.com>
2023-03-18 10:41:04 +0530

    swagger integration

--- src/routes/loginRoute.js
+++ src/routes/loginRoute.js
@@ -13,6 +13,15 @@ router.post('/login', loginValidator(), (req, res, next) => {
     loginController.login(req, res, next);
 });
 
+/**
+ * @openapi
+ * /login:
+ *   get:
+ *     description: Welcome to swagger-jsdoc!
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
 router.get('/verify-token', loginController.verifyToken);
 router.post('/refresh-token', loginController.refreshToken);
 
